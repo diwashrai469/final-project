f221b41db17d8c7a54320227f46d29c984eacd83
Diwash Rai <raidiwash30@gmail.com>
2023-01-12 22:12:18 +0545

    new commits

--- lib/login/signin folder/loginpage.dart
+++ lib/login/signin folder/loginpage.dart
@@ -10,6 +10,7 @@ class loginpage extends StatefulWidget {
 }
 
 class _loginpageState extends State<loginpage> {
+  final formkey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -23,87 +24,100 @@ class _loginpageState extends State<loginpage> {
               )),
               child: Padding(
                 padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    const Text(
-                      "Login",
-                      style: TextStyle(
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    Card(
-                      child: TextFormField(
-                        decoration: const InputDecoration(
+                child: Form(
+                  key: formkey,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      const Text(
+                        "Login",
+                        style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Card(
+                        child: TextFormField(
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return "Enter cannot be empty.";
+                            }
+                          },
+                          decoration: const InputDecoration(
+                              border: InputBorder.none,
+                              icon: Padding(
+                                padding: EdgeInsets.all(8.0),
+                                child: Icon(Icons.person),
+                              ),
+                              labelText: "User Email"),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      Card(
+                        child: TextFormField(
+                          obscureText: true,
+                          decoration: const InputDecoration(
                             border: InputBorder.none,
                             icon: Padding(
                               padding: EdgeInsets.all(8.0),
-                              child: Icon(Icons.person),
+                              child: Icon(Icons.lock),
                             ),
-                            labelText: "User Email"),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 5,
-                    ),
-                    Card(
-                      child: TextFormField(
-                        obscureText: true,
-                        decoration: const InputDecoration(
-                          border: InputBorder.none,
-                          icon: Padding(
-                            padding: EdgeInsets.all(8.0),
-                            child: Icon(Icons.lock),
+                            labelText: "Password",
                           ),
-                          labelText: "Password",
                         ),
                       ),
-                    ),
-                    ElevatedButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => mymap()),
-                          );
-                        },
-                        child: const Text(
-                          "LOG IN",
-                        )),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    const Divider(
-                      color: Colors.white,
-                      height: 2,
-                      endIndent: 30,
-                      indent: 30,
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    const Text(
-                      "Don't have an account?",
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                          fontSize: 15),
-                    ),
-                    ElevatedButton(
-                        onPressed: () {
-                          setState(() {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => signin()),
-                            );
-                          });
-                        },
-                        child: const Text("SIGN UP"))
-                  ],
+                      ElevatedButton(
+                          onPressed: () {
+                            final isvvalided = formkey.currentState!.validate();
+                            if (isvvalided) {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => mymap()),
+                              );
+                            }
+                          },
+                          child: const Text(
+                            "LOG IN",
+                          )),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      const Divider(
+                        color: Colors.white,
+                        height: 2,
+                        endIndent: 30,
+                        indent: 30,
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const Text(
+                        "Don't have an account?",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            fontSize: 15),
+                      ),
+                      ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => signin()),
+                              );
+                            });
+                          },
+                          child: const Text("SIGN UP"))
+                    ],
+                  ),
                 ),
               )),
         ),
